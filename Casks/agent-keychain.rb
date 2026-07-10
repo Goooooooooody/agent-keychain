092897f9c9a4fa1f3e6ec69f6ec309821f04be1d
Goooooooooody <205637002+Goooooooooody@users.noreply.github.com>
2026-07-10 12:35:33 +0100

    chore(homebrew): update cask for v0.2.0

--- Casks/agent-keychain.rb
+++ Casks/agent-keychain.rb
@@ -1,8 +1,8 @@
 cask "agent-keychain" do
-  version "0.1.2"
+  version "0.2.0"
   # Update this digest from akc-aarch64-apple-darwin.tar.gz.sha256 in the
   # corresponding GitHub release whenever version changes.
-  sha256 "adcfc32d4927b21706e149f1ca332ad3f5fa0b230299c2de03166631077ff226"
+  sha256 "49867d7b7e778ab85658ccd50b8705503c7b3e932f33515dfe134b5ee531380b"
 
   url "https://github.com/Goooooooooody/agent-keychain/releases/download/v#{version}/akc-aarch64-apple-darwin.tar.gz"
   name "Agent Keychain"
